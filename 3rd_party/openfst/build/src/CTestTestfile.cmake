4df07eab6b16a6557416dcde0fbb4f93f8fd3f5f
ChaoII <849453582@qq.com>
2025-01-10 18:31:40 +0800

    add asr_wss

---
+++ 3rd_party/openfst/build/src/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/AC/CLionProjects/wrzs_hys/3rd_party/openfst/src
+# Build directory: C:/Users/AC/CLionProjects/wrzs_hys/3rd_party/openfst/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("extensions")
+subdirs("test")
---
+++ 3rd_party/openfst/build/src/extensions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/AC/CLionProjects/wrzs_hys/3rd_party/openfst/src/extensions
+# Build directory: C:/Users/AC/CLionProjects/wrzs_hys/3rd_party/openfst/build/src/extensions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
