8b3e5d9e05f2a46538508aea41295a8a9db45e4e
Ai Chao <849453582@qq.com>
2025-06-02 13:34:17 +0800

    add vis pose

--- csrc/vision/common/visualize/visualize.cpp
+++ csrc/vision/common/visualize/visualize.cpp
@@ -5,7 +5,8 @@
 #include <random>
 #include "csrc/vision/common/visualize/visualize.h"
 
-namespace modeldeploy::vision {
+namespace modeldeploy::vision
+{
     cv::Scalar get_random_color() {
         std::random_device rd; // 获取随机数种子
         std::mt19937 gen(rd()); // 使用Mersenne Twister算法生成随机数
@@ -15,5 +16,30 @@ namespace modeldeploy::vision {
             static_cast<double>(dis(gen)),
             static_cast<double>(dis(gen))
         };
+    }
+
+    void draw_rectangle_and_text(cv::Mat& image, const cv::Rect2f box, const std::string& text,
+                                 const cv::Scalar& color, cv::FontFace font, const int font_size,
+                                 const int thickness, const bool draw_text) {
+        // 绘制对象矩形框
+        cv::rectangle(image, box, color, thickness);
+        const auto size = cv::getTextSize(cv::Size(0, 0),
+                                          text, cv::Point2f(box.x, box.y), font, font_size);
+        // 绘制标签背景
+        cv::rectangle(image, size, color, thickness);
+        if (draw_text) {
+            cv::putText(image, text, cv::Point2f(box.x, box.y - 2),
+                        cv::Scalar(255 - color[0], 255 - color[1], 255 - color[2]),
+                        font, font_size);
+        }
+    }
+
+    void draw_landmarks(cv::Mat& cv_image,
+                        const std::vector<cv::Point2f>& landmarks,
+                        const int landmark_radius) {
+        for (auto& landmark : landmarks) {
+            auto landmark_color = get_random_color();
+            cv::circle(cv_image, landmark, landmark_radius, landmark_color, -1);
+        }
     }
 }
