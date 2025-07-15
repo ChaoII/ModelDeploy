b13bca05efa50d42ae1c2bafacf2579f9e205a35
aichao <849453582@qq.com>
2025-07-15 15:59:00 +0800

    update ortbackend

--- csrc/runtime/backends/ort/ort.h
+++ csrc/runtime/backends/ort/ort.h
@@ -8,24 +8,19 @@
 #include <string>
 #include <vector>
 #include "csrc/core/md_decl.h"
-#include "onnxruntime_cxx_api.h"
 #include "csrc/runtime/backends/backend.h"
 #include "csrc/runtime/backends/ort/option.h"
 
 namespace modeldeploy {
-    struct OrtValueInfo {
-        std::string name;
-        std::vector<int64_t> shape;
-        ONNXTensorElementDataType dtype;
-    };
+    class OrtBackendImpl;
 
     class MODELDEPLOY_CXX_EXPORT OrtBackend : public BaseBackend {
     public:
-        OrtBackend() = default;
+        OrtBackend();
 
-        ~OrtBackend() override = default;
+        ~OrtBackend() override;
 
-        void build_option(const OrtBackendOption& option);
+        void build_option(const OrtBackendOption& option) const;
 
         bool init(const RuntimeOption& option) override;
 
@@ -34,29 +29,21 @@ namespace modeldeploy {
         std::unique_ptr<BaseBackend> clone(RuntimeOption& runtime_option,
                                            void* stream = nullptr, int device_id = -1) override;
 
-        [[nodiscard]] size_t num_inputs() const override { return inputs_desc_.size(); }
+        [[nodiscard]] size_t num_inputs() const override;
 
-        [[nodiscard]] size_t num_outputs() const override { return outputs_desc_.size(); }
+        [[nodiscard]] size_t num_outputs() const override;
 
         TensorInfo get_input_info(int index) override;
+
         TensorInfo get_output_info(int index) override;
+
         std::vector<TensorInfo> get_input_infos() override;
+
         std::vector<TensorInfo> get_output_infos() override;
+
         [[nodiscard]] std::map<std::string, std::string> get_custom_meta_data() const override;
 
     private:
-        bool init_from_onnx(const std::string& model_buffer,
-                            const OrtBackendOption& option = OrtBackendOption());
-
-        Ort::Env env_{ORT_LOGGING_LEVEL_WARNING, "MD"};
-        std::shared_ptr<Ort::Session> shared_session_{nullptr};
-        Ort::SessionOptions session_options_;
-        std::unique_ptr<Ort::IoBinding> binding_;
-        std::vector<OrtValueInfo> inputs_desc_;
-        std::vector<OrtValueInfo> outputs_desc_;
-        bool initialized_ = false;
-        std::string model_file_name_;
-        std::string model_buffer_;
-        OrtBackendOption option_;
+        std::unique_ptr<OrtBackendImpl> impl_;
     };
 }
