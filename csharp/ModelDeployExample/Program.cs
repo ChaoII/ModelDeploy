bc26221cd7d38ea038d5417a3b9d36dda7b178b1
aichao <849453582@qq.com>
2026-08-26 08:24:32 +0800

    chore(csharp): pedattr 示例微调 (FontSize 10, ShowAttr false)

--- csharp/ModelDeployExample/Program.cs
+++ csharp/ModelDeployExample/Program.cs
@@ -152,7 +152,7 @@ static class Program
         Dictionary<int, string> dict = new Dictionary<int, string>
             { { 0, "傻话" }, { 1, "雷达" }, { 2, "哈拉" }, { 3, "糍粑" }, { 4, "索拉" } };
 
-        result.Draw(image, new DrawOptions { Threshold = 0.25, FontSize = 1, Alpha = 0.15, LabelMap = dict, AbnormalIds = new List<int> { 0, 1 } });
+        result.Draw(image, new DrawOptions { Threshold = 0.25, FontSize = 10, Alpha = 0.15, LabelMap = dict, AbnormalIds = new List<int> { 0, 1 },ShowAttr = false});
         image.Save("pedattr_annotated.jpg");
         image.Show();
         Console.WriteLine("visualized -> pedattr_annotated.jpg");
