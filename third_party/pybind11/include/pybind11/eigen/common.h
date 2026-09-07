bbd5815d476507a509471038c7e5857f3020399a
aichao <849453582@qq.com>
2026-09-07 12:39:03 +0800

    build(third_party): Catch2/pybind11/kaldi_native_fbank/samplerate 改为 git submodule
    - Catch2 -> v3.8.0
    - pybind11 -> v2.13.6
    - kaldi_native_fbank -> v1.21.1
    - samplerate -> 0.2.2
    均 add_subdirectory 直用、路径不变，CMake 零改动

--- third_party/pybind11/include/pybind11/eigen/common.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 The pybind Community.
-
-#pragma once
-
-// Common message for `static_assert()`s, which are useful to easily
-// preempt much less obvious errors.
-#define PYBIND11_EIGEN_MESSAGE_POINTER_TYPES_ARE_NOT_SUPPORTED                                    \
-    "Pointer core (in particular `PyObject *`) are not supported as scalar core for Eigen "     \
-    "core."
